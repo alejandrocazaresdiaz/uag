9dd99e0a90b9aa537b917e6c2329c5191d01bfbf
Alejandro Cazares <mcasarez@mx1.ibm.com>
2019-03-19 14:29:09 -0600

    I forgot this class but it's here :D

--- homework/AnalogClock7.java
+++ homework/AnalogClock7.java
@@ -6,4 +6,21 @@ package com.alexCazares;
  */
 public class AnalogClock7 {
 
+    public static void main(String[] args) {
+        int hour = Integer.parseInt(args[0]);
+        int minute = Integer.parseInt(args[1]);
+        new AnalogClock7().eval(hour, minute);
+    }
+
+    public void eval(int hour, int minute) {
+        //calculate the inner angle
+        int angleHour = (360 / 12) * hour;
+        int angleMinute = (360 / 60) * minute;
+        int angle = Math.abs(angleHour - angleMinute);
+//        System.out.println(angleHour);
+//        System.out.println(angleMinute);
+        System.out.println((angle > 180 ? 360 - angle : angle) + "°");
+    }
+
+
 }
